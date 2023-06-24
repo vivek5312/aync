6d4a00fec10718700135442efffb247039e301eb
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-06-24 15:02:01 +0530

    all set

--- new1.js
+++ new1.js
@@ -1,85 +1,51 @@
+const posts = [];
+let lastActivity = null;
 
-
-const posts=[];
-
-let lastactivity=null;
-
-function createPost(post){
-
-    return new Promise((resolve,reject)=>{
-
-        setTimeout(()=>{
-
-            posts.push(post)
-
-       resolve();
-
-        },1000)
-
-        
-
-    })
-
+function createPost(post) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+            resolve();
+        }, 1000);
+    });
 }
 
-
-
-function lastActivityTime(){
-
-    return new Promise((resolve,reject)=>{
-
-        setTimeout(()=>{
-
-            
-
-            resolve(lastactivity=new Date());
-
-        },1000)
-
-    })
-
+function getLastActivityTime() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            lastActivity = new Date();
+            resolve();
+        }, 1000);
+    });
 }
 
-function deletePost(){
-
-    return new Promise((resolve, reject)=>{
-
-        setTimeout(()=>{
-
-            if(posts.length>0){
-
-                let deletepost=posts.pop();
-
-                resolve(deletepost);
-
+function deletePost() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (posts.length > 0) {
+                const deletedPost = posts.pop();
+                resolve(deletedPost);
+            } else {
+                reject('Error: No post to delete');
             }
-
-            else{
-
-                reject("Error:No post delete")
-
-            }
-
-        },1000)
-
-        
-
-    })
-
+        }, 1000);
+    });
 }
 
+async function performOperations() {
+    try {
+        await createPost({ title: 'post1' });
+        await getLastActivityTime();
 
+        console.log('Posts:', posts);
+        console.log('Last Activity:', lastActivity);
 
-async function createPost(){
-console.log('Post',posts)
-let time=await lastActivityTime();
-let del=await deletePost();
- console.log(time);
- console.log(del);
-
+        const deletedPost = await deletePost();
+        console.log('Deleted Post:', deletedPost);
+        console.log('Updated Posts:', posts);
+    } catch (error) {
+        console.log('Error:', error);
+    }
 }
-createPost().then(()=>{
-    console.log('updatePost',posts);
-}).catch((err)=>{
-    console.log(err);
-})
+
+performOperations();
